b415f65b7fde52df3af157a1e04cf25046d566a5
rodrigoeiras <rodrigo.eiras@live.com>
2017-09-11 16:33:27 -0300

    Fetch YouTube profiles from Social Bakers API

---
+++ R_getYouTubeProfilesFromSB.R
@@ -0,0 +1,37 @@
+# R Connector to Social Bakers API
+# Function to fetch all registered profiles
+# In this function you set variable to fetch a different network from Social Bakers.
+## in my case, i only need YouTube.
+# Made by Rodrigo Eiras - rsveiras@gmail.com
+# 05/09/2017 - Rio de Janeiro / RJ
+
+
+library(httr)
+library(RCurl)
+library(jsonlite)
+
+j <- NULL
+ids <- NULL
+name <- NULL
+profileName <- NULL
+timezone <- NULL
+
+req <- GET("https://api.socialbakers.com/0/youtube/profiles", 
+           authenticate("login",
+                        "passwd",
+                        type = "basic"),
+           encode = "json")
+
+stop_for_status(req)
+data <- content(req, as = "parsed")
+
+
+for(j in 1:length(data$profiles)){
+  
+  ids[j] <- toString(unlist(data$profiles[j][[1]]$id))
+  name[j] <- toString(unlist(data$profiles[j][[1]]$name))
+  profileName[j] <- toString(unlist(data$profiles[j][[1]]$profileName))
+  timezone[j] <- toString(unlist(data$profiles[j][[1]]$timezone))
+}
+
+YTprofilesTable <- data.frame(id = ids, name, profileName, timezone)
